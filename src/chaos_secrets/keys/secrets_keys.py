9ec0a1d4b930d533a97ce561c41928a7829e5498
Caio Rocha de Oliveira <caiorocoli@gmail.com>
2026-01-20 21:05:35 -0300

    keys

---
+++ src/chaos_secrets/keys/secrets_keys.py
@@ -0,0 +1,16 @@
+secrets_chobolo_keys = [
+    {
+        "secrets": {
+            "templates": [
+                {
+                    "from": "",
+                    "to": "",
+                    "owner": "",
+                    "mode": "",
+                    "escape": False,
+                    "vars": []
+                }
+            ]
+        }
+    }
+]
